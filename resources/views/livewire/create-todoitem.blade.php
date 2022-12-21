acddb97b11075799f55be27589b59c5189e45d8b
vaste2408 <ois.vasiljev.aa@mail.ru>
2022-12-21 17:39:48 +1000

    Todolist v0.6 some styles

--- resources/views/livewire/create-todoitem.blade.php
+++ resources/views/livewire/create-todoitem.blade.php
@@ -1,6 +1,6 @@
 <div class="todoitem-create">
     <form wire:submit.prevent="save">
-        <input type="text" wire:model.lazy="text" placeholder="Type to create new">
+        <input type="text" wire:model.lazy="text" placeholder="Type to create new item">
         <button type="submit">Add</button>
         @error('text') <span class="error">{{ $message }}</span> @enderror
     </form>
